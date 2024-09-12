9977e23894795196d8577e014e56cb8399b3b66a
bobo <126691@qq.com>
2024-09-12 17:45:38 +0800

    新增LayoutShow

--- LayoutShow/app/src/main/java/com/example/layoutshow/RelativeActivity.kt
+++ LayoutShow/app/src/main/java/com/example/layoutshow/RelativeActivity.kt
@@ -1,10 +1,7 @@
 package com.example.layoutshow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class RelativeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- LayoutShow/app/src/main/java/com/example/layoutshow/TableActivity.kt
+++ LayoutShow/app/src/main/java/com/example/layoutshow/TableActivity.kt
@@ -1,10 +1,7 @@
 package com.example.layoutshow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
